75a215083fdfc5c0ee818e1eefc0765f45910fc5
Mujahid-Shariff <122116016+Mujahid-Shariff@users.noreply.github.com>
2023-02-05 01:50:52 +0530

    Add files via upload

---
+++ Hotel_review_stop_words.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 01:10:14 2023
+
+@author: admin
+"""
+
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+ps = PorterStemmer()
+
+
+stopwords = stopwords.words("english")
+stopwords.remove("not")
+stopwords.remove("no")
+
+#stopwords
+# Remove a specific word from the stopwords list
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+    
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Hotel Reviews Prediction")
+
+input_review = st.text_area("Please enter your review here")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_review = transform_text(input_review)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_review])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Yayy!! Positive Review")
+    else:
+        st.header("Negative Review :(")
